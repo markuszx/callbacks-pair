f0649398d93e6fddc870bc8f5f04b2df73010619
mark garcia <markkusag@outlook.com>
2023-08-11 18:26:35 -0500

    11 complete

--- problems/11-and-select.js
+++ problems/11-and-select.js
@@ -30,6 +30,11 @@ console.log(andSelect(['ants', 'APPLES', 'ART', 'BACON', 'arm'], isUpperCase,  s
 
 function andSelect(array, cb1, cb2) {
   // Your code here
+  let newArr = [];
+  for(let el of array){
+    if(cb1(el) && cb2(el)){newArr.push(el)}
+  }
+  return newArr
 }
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
