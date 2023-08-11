efb287bd9dabf326d59a56a919c8c453eb330998
mark garcia <markkusag@outlook.com>
2023-08-11 17:11:34 -0500

    04 complete

--- problems/04-my-filter.js
+++ problems/04-my-filter.js
@@ -21,11 +21,15 @@ console.log(result2);      // ['choose', 'words', 'only']
 
 function myFilter(array, cb) {
     // Your code here
+    let arr = []
+    for (let i = 0; i < array.length; i++) {
+   if (cb(array[i])) { arr.push(array[i]) } }
+   return arr
 }
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
     module.exports = myFilter;
-} catch(e) {
+} catch (e) {
     return null;
 }
