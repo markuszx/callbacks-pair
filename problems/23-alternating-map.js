0c746195d4e94cde3c89f8259b8c6f3e59daf85e
WeiCheng <drchenweicheng@gmail.com>
2023-08-15 20:36:23 -0700

    problem 23

--- problems/23-alternating-map.js
+++ problems/23-alternating-map.js
@@ -36,11 +36,39 @@ console.log(alternatingMap(['hEy', 'EVERYone', 'whats', 'uP??'], yell, whisper))
 
 function alternatingMap(array, cb1, cb2) {
   // Your code here
+  let result = []
+  for (let i = 0; i < array.length; i++) {
+    if (i % 2 === 0) {
+      result.push(cb1(array[i]))
+    } else {
+      result.push(cb2(array[i]))
+    }
+  }
+  return result;
 }
+let triple = function (n) {
+  return 3 * n;
+};
 
+let half = function (n) {
+  return n / 2;
+};
+console.log(alternatingMap([7, 3, 2, 9, 8], triple, half));
+// [ 21, 1.5, 6, 4.5, 24 ]
+
+
+let yell = function (s) {
+  return s.toUpperCase() + '!';
+};
+
+let whisper = function (s) {
+  return '..' + s.toLowerCase() + '..';
+};
+console.log(alternatingMap(['hEy', 'EVERYone', 'whats', 'uP??'], yell, whisper));
+// [ 'HEY!', '..everyone..', 'WHATS!', '..up??..' ]
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
   module.exports = alternatingMap;
-} catch(e) {
+} catch (e) {
   return null;
 }
