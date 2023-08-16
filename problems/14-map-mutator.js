29f728319efdd6c1cc563aada9500fcf5eecc6da
WeiCheng <drchenweicheng@gmail.com>
2023-08-15 19:35:50 -0700

    problem 14

--- problems/14-map-mutator.js
+++ problems/14-map-mutator.js
@@ -5,26 +5,28 @@ to overwrite elements of the original array, mutating the array.
 
 Examples:
 
+
+*******************************************************************************/
+
+function mapMutator(array, cb) {
+  for (let i = 0; i < array.length; i++) {
+    array[i] = cb(array[i], i)
+  }
+}
 let arr1 = [4, 2, 6, 5];
 mapMutator(arr1, function (el) {
-    return el * 2;
+  return el * 2;
 });
 console.log(arr1);  // [ 8, 4, 12, 10 ]
 
 let arr2 = [8, 9, 10];
 mapMutator(arr2, function (el, i) {
-    return el * i;
+  return el * i;
 });
 console.log(arr2); // [ 0, 9, 20 ]
-*******************************************************************************/
-
-function mapMutator(array, cb) {
-  // Your code here
-}
-
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
   module.exports = mapMutator;
-} catch(e) {
+} catch (e) {
   return null;
 }
