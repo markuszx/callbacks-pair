34e1ecdf3e03eda61665c1dd5d268b38d973de8b
WeiCheng <drchenweicheng@gmail.com>
2023-08-15 19:30:28 -0700

    problem 13

--- problems/13-min-value-callback.js
+++ problems/13-min-value-callback.js
@@ -11,12 +11,18 @@ console.log(minValueCallback([64, 25, 49, 9, 100], Math.sqrt));  // 3
 *******************************************************************************/
 
 function minValueCallback(array, cb) {
-    // Your code here
+  // Your code here
+  let minValue = Math.min(...array);
+  if (cb) {
+    return cb(minValue);
+  } else {
+    return minValue
+  }
 }
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
   module.exports = minValueCallback;
-} catch(e) {
+} catch (e) {
   return null;
 }
